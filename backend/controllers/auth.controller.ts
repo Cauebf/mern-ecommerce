ab59a6080deb8968e4e517432602a61482037ca7
Cauê Favarim <cauebrolesef@gmail.com>
2025-08-23 15:56:21 -0300

    added user model and hash password

--- backend/controllers/auth.controller.ts
+++ backend/controllers/auth.controller.ts
@@ -1,7 +1,28 @@
 import type { Request, Response } from "express";
+import User from "../models/user.model.js";
 
 export const signup = async (req: Request, res: Response) => {
-  res.send("signup route");
+  const { name, email, password } = req.body;
+
+  try {
+    const userExists = await User.findOne({ email });
+
+    if (userExists) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+
+    const user = await User.create({ name, email, password });
+
+    res.status(201).json({ user, message: "User created successfully" });
+  } catch (error) {
+    if (error instanceof Error) {
+      console.error("Error creating user:", error);
+      res.status(500).json({ message: error.message });
+    } else {
+      console.error("Unexpected error:", error);
+      res.status(500).json({ message: "An unexpected error occurred" });
+    }
+  }
 };
 
 export const login = async (req: Request, res: Response) => {
