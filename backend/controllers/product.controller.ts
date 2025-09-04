703270f1c9bdb3e44447164f6cb6dff01958ac93
Cauê Favarim <cauebrolesef@gmail.com>
2025-09-04 15:45:21 -0300

    add getFeaturedProducts controller

--- backend/controllers/product.controller.ts
+++ backend/controllers/product.controller.ts
@@ -1,5 +1,6 @@
 import type { Request, Response } from "express";
 import Product from "../models/product.model.js";
+import redis from "../lib/redis.js";
 
 export const getAllProducts = async (req: Request, res: Response) => {
   try {
@@ -7,6 +8,38 @@ export const getAllProducts = async (req: Request, res: Response) => {
     res.status(200).json({ products });
   } catch (error) {
     console.error("Error getting products:", error);
+    const message =
+      error instanceof Error ? error.message : "Internal server error";
+    res.status(500).json({ message });
+  }
+};
+
+export const getFeaturedProducts = async (req: Request, res: Response) => {
+  try {
+    // get featured products from redis and return if found
+    const featuredProducts = await redis.get("featured_products");
+    if (featuredProducts) {
+      return res.json(JSON.parse(featuredProducts)); // parse() to convert JSON string to javascript object
+    }
+
+    // if not in redis, get from database and store in redis
+    const featuredProductsDb = await Product.find({ isFeatured: true }).lean(); // lean() to get plain javascript objects instead of mongoose documents (which improves performance)
+
+    if (!featuredProductsDb) {
+      return res.status(404).json({ message: "No featured products found" });
+    }
+
+    // store in redis for future quick access
+    await redis.set(
+      "featured_products",
+      JSON.stringify(featuredProductsDb), // stringify() to convert javascript object to JSON string
+      "EX",
+      60 * 60 * 24 // 1 day
+    );
+
+    res.json(featuredProductsDb);
+  } catch (error) {
+    console.error("Error getting featured products:", error);
     const message =
       error instanceof Error ? error.message : "Internal server error";
     res.status(500).json({ message });
--- backend/routes/product.route.ts
+++ backend/routes/product.route.ts
@@ -1,9 +1,13 @@
 import express from "express";
-import { getAllProducts } from "../controllers/product.controller.js";
+import {
+  getAllProducts,
+  getFeaturedProducts,
+} from "../controllers/product.controller.js";
 import { adminRoute, protectedRoute } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
 router.get("/", protectedRoute, adminRoute, getAllProducts);
+router.get("/featured", getFeaturedProducts);
 
 export default router;
